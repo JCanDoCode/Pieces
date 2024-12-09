0418bc03c8899e7b4f78ca6e9ab948db730e0948
JCanDoCode <156962029+JCanDoCode@users.noreply.github.com>
2024-12-09 13:27:48 -0800

    Version 0.0.8
    Puzzle css cleanup and functional updates

--- js/puzzle1.js
+++ js/puzzle1.js
@@ -1,24 +1,45 @@
 const boardCords = [...document.querySelectorAll(".board-cords")];
-const playable = [...document.querySelectorAll(".playable")];
+const white = [...document.querySelectorAll(".white")];
 
-const placePiece= (e) => {
-    switch(e.target) {
-        case h5:
-            h5.style.backgroundColor = "yellow";
-            break;
-        case g5:
-            g5.style.backgroundColor = "yellow";
-            break;
-        case g3:
-            g3.style.backgroundColor = "yellow";
-            break;
-        case e8:
-            e8.style.backgroundColor = "yellow";
-            break;
-        default:
-            playable.forEach(grid => {
-                grid.removeAttribute("style");
-            });
+const placePiece = async (e) => {
+    if(e.target = white) {
+        boardCords.forEach(grid => {
+            grid.removeAttribute("style");
+            grid.classList.remove("legal-move");
+        });
+        switch(e.target) {
+            case h5:
+                h5.style.backgroundColor = "#ffca38";
+                h6.classList.add("legal-move");
+                g6.classList.add("legal-move");
+                await h6.addEventListener("click", (e) => {
+                    e.target.style.backgroundColor = "green";
+                    e.target.classList.remove("black")
+                    e.target.classList.add("pawn", "white");
+                    h5.classList.remove("pawn");
+                });
+                await g6.addEventListener("click", () => g6.style.backgroundColor = "red");
+                break;
+            case g5:
+                g5.style.backgroundColor = "#ffca38";
+                break;
+            case g3:
+                g3.style.backgroundColor = "#ffca38";
+                break;
+            case e8:
+                e8.style.backgroundColor = "#ffca38";
+                break;
+            default:
+                boardCords.forEach(grid => {
+                    grid.removeAttribute("style");
+                    grid.classList.remove("legal-move");
+                });
+        }
+    } else {
+        boardCords.forEach(grid => {
+            grid.removeAttribute("style");
+            grid.classList.remove("legal-move");
+        });
     }
 }
 

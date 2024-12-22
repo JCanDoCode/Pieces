31ca287139bd755449e1051b53b3d24c6f89b699
JCanDoCode <156962029+JCanDoCode@users.noreply.github.com>
2024-12-21 23:23:22 -0800

    Version 1.0.0
    Puzzle 1 finished.

--- js/puzzle1.js
+++ js/puzzle1.js
@@ -22,12 +22,69 @@ const placePiece = async (e) => {
                 break;
             case g5:
                 g5.style.backgroundColor = "#ffca38";
+                h6.classList.add("legal-move");
+                h4.classList.add("legal-move");
+                f6.classList.add("legal-move");
+                f4.classList.add("legal-move");
+                e7.classList.add("legal-move");
+                e3.classList.add("legal-move");
+                d8.classList.add("legal-move");
+                d2.classList.add("legal-move");
+                c1.classList.add("legal-move");
+                await boardCords.forEach(grid => {
+                    grid.addEventListener("click", () => {
+                        g5.style.backgroundColor = "red";
+                        setTimeout(() => {
+                            e.target.removeAttribute("style");
+                            e.target.classList.remove("legal-move");
+                        }, 1000);
+                    });
+                });
                 break;
             case g3:
                 g3.style.backgroundColor = "#ffca38";
+                f3.classList.add("legal-move");
+                f4.classList.add("legal-move");
+                g4.classList.add("legal-move");
+                g2.classList.add("legal-move");
+                h4.classList.add("legal-move");
+                h3.classList.add("legal-move");
+                h2.classList.add("legal-move");
+                await boardCords.forEach(grid => {
+                    grid.addEventListener("click", () => {
+                        g3.style.backgroundColor = "red";
+                        setTimeout(() => {
+                            e.target.removeAttribute("style");
+                            e.target.classList.remove("legal-move");
+                        }, 1000);
+                    });
+                });
                 break;
             case e8:
                 e8.style.backgroundColor = "#ffca38";
+                a8.classList.add("legal-move");
+                b8.classList.add("legal-move");
+                c8.classList.add("legal-move");
+                d8.classList.add("legal-move");
+                f8.classList.add("legal-move");
+                g8.classList.add("legal-move");
+                h8.classList.add("legal-move");
+                e7.classList.add("legal-move");
+                e6.classList.add("legal-move");
+                e5.classList.add("legal-move");
+                e4.classList.add("legal-move");
+                e3.classList.add("legal-move");
+                e2.classList.add("legal-move");
+                e1.classList.add("legal-move");
+                await boardCords.forEach(grid => {
+                    grid.addEventListener("click", () => {
+                        e8.style.backgroundColor = "red";
+                        setTimeout(() => {
+                            e.target.removeAttribute("style");
+                            e.target.classList.remove("legal-move");
+                        }, 1000);
+                    });
+                });
                 break;
             default:
                 boardCords.forEach(grid => {
